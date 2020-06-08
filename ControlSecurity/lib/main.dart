91a57d0a9cbadc241225b2003ffd58fdae2ac82c
NoTeX25 <25mbattle25@gmail.com>
2020-06-08 16:53:50 -0600

    creating splash page

---
+++ ControlSecurity/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ControlSecurity());
+}
+
+class ControlSecurity extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  //Fields in a Widget subclass are always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+//create container widgets here:
+Widget _buildTopTransition(){
+  return Container(
+    color: Colors.black,
+    child: Image.asset("assets/images/cs-green.png"),
+    //green logo goes here
+  );
+}
+Widget _buildBottomTransition(){
+  return Container(
+    color: Colors.green[700],
+  );
+}
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            //stack container widgets here bottom to top
+            _buildBottomTransition(),
+            _buildTopTransition(),
+          ],
+        ),
+      )
+    );
+  }
+}
