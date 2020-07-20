92015f6f0872d9fe633241d3fae407a2db69a64c
NoTeX25 <25mbattle25@gmail.com>
2020-07-19 22:52:18 -0600

    created new TabBar and TabBar view (requires update)

--- ControlSecurity/lib/dashboard.dart
+++ ControlSecurity/lib/dashboard.dart
@@ -11,39 +11,72 @@ class Dashboard extends StatefulWidget{
   }
 }
 
-//Controllers will be stored here:
-
-
-
-
-//basic skeleton of buttons used for dashboard
-//Build widgets here: 
-Widget _buildContainer(){
-  return SizedBox.expand(
-    child: Container(
-      color: Colors.teal,
-    ),
-  );
-}
 
 class DashboardState extends State<Dashboard>{
 
-  @override
   Widget build(BuildContext context){
-    return SizedBox.expand(
-      
-        child: DefaultTabController(
-        length: 3, //number of tabs
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.note)),
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.calendar_today)),
-              ],
+              isScrollable: true,
+              tabs: choices.map((Choice choice){
+                return Tab(
+//                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
             ),
           ),
+          body: TabBarView(
+            children: choices.map((Choice choice){
+              return Container(
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        )
+      ),
+    );
+  }
+}
+
+
+class Choice{
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Notes', icon: Icons.note),
+  const Choice(title: 'Home', icon: Icons.home),
+  const Choice(title: 'Calendar', icon: Icons.calendar_today),
+];
+
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.white, //color will change this is just for a test
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
         ),
       ),
     );
