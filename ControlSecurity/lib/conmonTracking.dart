284c647e02d5a835bc156b86ca0431939d7cf9c5
NoTeX25 <25mbattle25@gmail.com>
2020-12-13 20:31:42 -0700

    removing that calendar example

--- ControlSecurity/lib/conmonTracking.dart
+++ ControlSecurity/lib/conmonTracking.dart
@@ -10,29 +10,9 @@ class ConmonTracking extends StatefulWidget{
   }
 }
 
-//Creating a global Device Calendar object here:
-List<Calendar> _calendars;
-Calendar _selectedCalendar;
-
 
 
 class ConmonTrackingState extends State<ConmonTracking>{
-DeviceCalendarPlugin _deviceCalendarPlugin;
-
-
-List<Calendar> _calendars;
-Calendar _selectedCalendar;
-
-ConmonTrackingState(){
-  _deviceCalendarPlugin = new DeviceCalendarPlugin();
-}
-
-  @override
-  initState() {
-    super.initState();
-    _retrieveCalendars();
-  }
-
 
   @override
   Widget build(BuildContext context){
@@ -41,28 +21,6 @@ ConmonTrackingState(){
         color: Colors.black,
       ),
     );
-  }
-
-  //retrieve calanders async function:
- void _retrieveCalendars() async {
-    //Retrieve user's calendars from mobile device
-    //Request permissions first if they haven't been granted
-    try {
-      var permissionsGranted = await _deviceCalendarPlugin.hasPermissions();
-      if (permissionsGranted.isSuccess && !permissionsGranted.data) {
-        permissionsGranted = await _deviceCalendarPlugin.requestPermissions();
-        if (!permissionsGranted.isSuccess || !permissionsGranted.data) {
-          return;
-        }
-      }
-
-      final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
-      setState(() {
-        _calendars = calendarsResult?.data;
-      });
-    } catch (e) {
-      print(e);
-    }
   }
 }
 
